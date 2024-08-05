851b506c0efb57d48e244137c43b603b12174f27
Duygu Tuncay <duygutuncay@hotmail.com>
2024-08-05 16:10:48 +0300

    a custom module : hospital management system - manifest file update

---
+++ custom_modules/hospital_management/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'author': 'Duygu Tuncay',
+    'website': 'www.duygutuncay.com',
+    'category': 'Health',
+    'summary': 'Hospital Management System',
+    'description': 'Hospital Management System - sample custom module',
+    'application': True,
+    'license': 'LGPL-3',
+}
